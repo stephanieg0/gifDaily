156a25dedde4dfae36890e57a54f34cf8e3f44de
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-03-18 20:34:24 -0500

    prepared template and controller for login

--- www/js/app.js
+++ www/js/app.js
@@ -65,12 +65,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.login', {
+    url: '/login',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-login': {
+        templateUrl: 'templates/tab-login.html',
+        controller: 'LoginCtrl'
       }
     }
   });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,13 +37,6 @@ angular.module('starter.controllers', [])
 })
 
 .controller('favoritesCtrl', function($scope, $http) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
   console.log('favorites controller is working');
   //call to my server
@@ -78,8 +71,7 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('LoginCtrl', function($scope) {
+
 });
+
